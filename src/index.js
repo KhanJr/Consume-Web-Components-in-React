3063b2904a5a618d9d442dc593fca3018551e2f0
KhanJr <khajr@gmail.com>
2021-10-06 14:47:54 +0530

    feat: Using web component in reactjs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 
-const MyCounter = lazy(() => import("./react-component"));
-
+// const MyCounter = lazy(() => import("./react-component"));
+const App = lazy(() => import("./App"));
 function MyComponent() {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <MyCounter name="Rizwan Khan" />
+        {/* <MyCounter name="Rizwan Khan" /> */}
+        <App />
       </Suspense>
     </div>
   );
